8a17643e9004793011b39891561c19a08dbd1008
Thanh Nguyen <thanhnhotn2001@gmail.com>
2023-04-13 00:10:14 +0900

    pintOS

--- src/threads/synch.c
+++ src/threads/synch.c
@@ -68,7 +68,7 @@ sema_down (struct semaphore *sema)
   old_level = intr_disable ();
   while (sema->value == 0) 
     {
-      list_push_back (&sema->waiters, &thread_current ()->elem);
+      list_insert_ordered(&sema -> waiters, &thread_current() -> elem, cmp_priority, NULL);
       thread_block ();
     }
   sema->value--;
@@ -113,10 +113,12 @@ sema_up (struct semaphore *sema)
   ASSERT (sema != NULL);
 
   old_level = intr_disable ();
-  if (!list_empty (&sema->waiters)) 
-    thread_unblock (list_entry (list_pop_front (&sema->waiters),
-                                struct thread, elem));
+  if (!list_empty (&sema->waiters)){
+    list_sort(&sema -> waiters, cmp_priority, NULL);
+    thread_unblock (list_entry (list_pop_front (&sema->waiters), struct thread, elem));
+  }
   sema->value++;
+  priority_cmp_test();
   intr_set_level (old_level);
 }
 
@@ -295,7 +297,7 @@ cond_wait (struct condition *cond, struct lock *lock)
   ASSERT (lock_held_by_current_thread (lock));
   
   sema_init (&waiter.semaphore, 0);
-  list_push_back (&cond->waiters, &waiter.elem);
+  list_insert_ordered(&cond -> waiters, &waiter.elem, sema_cmp_priority, NULL);
   lock_release (lock);
   sema_down (&waiter.semaphore);
   lock_acquire (lock);
@@ -316,9 +318,10 @@ cond_signal (struct condition *cond, struct lock *lock UNUSED)
   ASSERT (!intr_context ());
   ASSERT (lock_held_by_current_thread (lock));
 
-  if (!list_empty (&cond->waiters)) 
-    sema_up (&list_entry (list_pop_front (&cond->waiters),
-                          struct semaphore_elem, elem)->semaphore);
+  if (!list_empty (&cond->waiters)){
+    list_sort(&cond -> waiters, sema_cmp_priority, NULL);
+    sema_up (&list_entry (list_pop_front (&cond->waiters), struct semaphore_elem, elem)->semaphore);
+  } 
 }
 
 /* Wakes up all threads, if any, waiting on COND (protected by
@@ -335,4 +338,18 @@ cond_broadcast (struct condition *cond, struct lock *lock)
 
   while (!list_empty (&cond->waiters))
     cond_signal (cond, lock);
+}
+
+bool 
+sema_cmp_priority(const struct list_elem *x, const struct list_elem *y, void *aux UNUSED){
+  struct semaphore_elem *x_sema = list_entry(x, struct semaphore_elem, elem);
+  struct semaphore_elem *y_sema = list_entry(y, struct semaphore_elem, elem);
+
+  struct list *x_waiter = &(x_sema -> semaphore.waiters);
+  struct list *y_waiter = &(y_sema -> semaphore.waiters);
+
+  struct thread *x_thread = list_entry(list_begin(x_waiter), struct thread, elem);
+  struct thread *y_thread = list_entry(list_begin(y_waiter), struct thread, elem);
+
+  return x_thread -> priority > y_thread -> priority;
 }
--- src/threads/synch.h
+++ src/threads/synch.h
@@ -40,7 +40,7 @@ void cond_init (struct condition *);
 void cond_wait (struct condition *, struct lock *);
 void cond_signal (struct condition *, struct lock *);
 void cond_broadcast (struct condition *, struct lock *);
-
+bool sema_cmp_priority(const struct list_elem *x, const struct list_elem *y, void *aux UNUSED);
 /* Optimization barrier.
 
    The compiler will not reorder operations across an
